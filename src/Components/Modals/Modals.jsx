2faeaefaa9da5371da37bc870af7d35250237f28
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2024-04-11 15:43:34 -0300

    agregue json con informacion de las skills y un modals para mostrarlos

---
+++ src/Components/Modals/Modals.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import "./modals.css"
+
+
+function Modals({imagen, info}) {
+  const [show, setShow] = useState(false);
+
+  console.log("props", imagen)
+  console.log("info", info)
+
+  return (
+    <>
+      <img src={imagen} onClick={() => setShow(true)}/>
+
+
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            HOLA {info.name}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
+            commodi aspernatur enim, consectetur. Cumque deleniti temporibus
+            ipsam atque a dolores quisquam quisquam adipisci possimus
+            laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
+            accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
+            reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
+            deleniti rem!
+          </p>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+export default Modals;
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -4,6 +4,7 @@ import htmlIcon from "../../../public/icons/HTML.svg"
 import CSSIcon from "../../../public/icons/CSS.svg"
 import JSIcon from "../../../public/icons/JS.svg"
 import bootstrapIcon from "../../../public/icons/bootstrapColor.svg" 
+import reactBootstrapicon from "../../../public/icons/React-Bootstrap.svg"
 import reactIcon from "../../../public/icons/React.svg"
 import ejsIcon from "../../../public/icons/ejs-svgrepo-com.svg"
 import reduxIcon from "../../../public/icons/ReduxColor.svg"
@@ -14,34 +15,37 @@ import expressicon from "../../../public/icons/Express.svg"
 import sequelizeicon from "../../../public/icons/sequelizejs-icon.svg"
 import postgresqlIcon from "../../../public/icons/postgresql-icon.svg"
 import MysqlIcon from "../../../public/icons/mysql-icon.svg"
-
-
+import Modals from "../Modals/Modals";
+import data from "../../data/skills.json"
 
 
 export default function Skills(){
-
+console.log(data)
 
     return(
 
         <div className="icons">
 
-            <img src={htmlIcon} alt="HTML ICON" />
-            <img src={CSSIcon} alt="CSS ICON" />
-            <img src={JSIcon} alt="JS ICON" />
-            <img src={bootstrapIcon} alt="BOOTSTRAP ICON" />
-            <img src={reactIcon} alt="reactIcon" />
-            <img src={ejsIcon} alt="EJSicon" />
-            <img src={reduxIcon} alt="ReduxIcon" />
-            <img src={viteicons} alt="Vite icon" />
-            <img src={nodeIcon} alt="nodejs icon" />
-            <img src={expressicon} alt="express icon" />
-            <img src={sequelizeicon} alt="sequelize icon" />
-            <img src={postgresqlIcon} alt="postresql icon" />
-            <img src={MysqlIcon} alt="mysql icon" />
-            <img src={sqlicon} alt="sql icon" />
+            <Modals imagen={htmlIcon} info={data[0]}/>
+            {/* <Modals imagen={CSSIcon} info={data[0]/>
+            <Modals imagen={JSIcon} info={data[0]/>
+            <Modals imagen={bootstrapIcon} info={data[0]/>
+            <Modals imagen={reactBootstrapicon} />
+            <Modals imagen={reactIcon}/>
+            <Modals imagen={ejsIcon}/>
+            <Modals imagen={reduxIcon}/>
+            <Modals imagen={viteicons}/>
+            <Modals imagen={nodeIcon}/>
+            <Modals imagen={expressicon}/>
+            <Modals imagen={sequelizeicon}/>
+            <Modals imagen={postgresqlIcon}/>
+            <Modals imagen={MysqlIcon}/>
+            <Modals imagen={sqlicon}/>
 
+         */}
         </div>
-        
+
+       
     );
   } 
   
