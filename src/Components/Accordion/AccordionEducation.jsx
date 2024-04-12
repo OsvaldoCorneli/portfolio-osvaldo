0ead1ae6c8d7e996dbeaf3faf19cba245cf00da4
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2024-04-12 15:34:05 -0300

    agregue accordion para los cursos cortos y modifique los box con @media

---
+++ src/Components/Accordion/AccordionEducation.jsx
@@ -0,0 +1,55 @@
+import Accordion from 'react-bootstrap/Accordion';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './AccordionEducation.css'
+import codigoFacilito from "../../../public/Logos-educacion/CodigoFacilito.jpg"
+import desafioLatam from "../../../public/Logos-educacion/DesafioLatam.png"
+import ticmas from "../../../public/Logos-educacion/Ticmas.jpg"
+import linkedIn from "../../../public/Logos-educacion/LinkedIn.png"
+function AccordionEducation() {
+  return (
+    <Accordion defaultActiveKey={['']} alwaysOpen>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header><img src={codigoFacilito} alt="" /> Codigo Facilito</Accordion.Header>
+        <Accordion.Body>
+          <ul>
+            <li>Curso de JavaScript y el DOM</li>
+            <li>Curso profesional de Git</li>
+            <li>Curso profesional de Typescript</li>
+            <li>Curso de fundamentos de UX para programadores</li>
+          </ul>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header><img src={desafioLatam} alt="" /> Desafio Latam</Accordion.Header>
+        <Accordion.Body>
+          <ul>
+            <li>Bases de GIT,GITHUB</li>
+            <li>CSS Avanzado</li>
+            <li>English for Developers & IT Professionals</li>
+          </ul>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header><img src={ticmas} alt="" /> Argentina Programa 4.0 / Ticmas</Accordion.Header>
+        <Accordion.Body>
+          <ul>
+            <li>Procesamiento de datos con Python</li>
+            <li>Primeros pasos del desarrollo FrontEnd</li>
+          </ul>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="3">
+        <Accordion.Header><img src={linkedIn} alt="" /> LinkedIn</Accordion.Header>
+        <Accordion.Body>
+          <ul>
+            <li>Fundamentos Esenciales de la programacion</li>
+            <li>Introduccion a las habilidades profesionales en el desarrollo de software</li>
+            <li>Fundamentos profesionales del desarrollo de software, por Microsoft y LinkedIn</li>
+          </ul>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+}
+
+export default AccordionEducation;
--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -6,19 +6,30 @@ import digital from '../../../public/Logos-educacion/DH.png'
 import henry from '../../../public/Logos-educacion/descarga.jpg'
 import CaC from '../../../public/Logos-educacion/CodoaCodo.png'
 import AP from '../../../public/Logos-educacion/Argentinaprograma.png'
-
+import AccordionEducation from "../Accordion/AccordionEducation";
 
 export default function Education(){
 
 
     return(
         <div id="education-body">
+            <div id="contenedor1-Educationbody">
+            <h5>Bootcamps</h5>
             <div id="cartas-educacion">
             <Card info={data[0]} imagen={CaC}/>
             <Card info={data[1]} imagen={digital}/>
             <Card info={data[2]} imagen={henry}/>
             <Card info={data[3]} imagen={AP}/>
             </div>
+            </div>
+            <div id="contenedor2-Educationbody">
+            <h5>Cursos Cortos</h5>
+            <div id="acordion">
+             <AccordionEducation/>
+            </div>
+            </div>
+           
+             
            
         </div>
     )
