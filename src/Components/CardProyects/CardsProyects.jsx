6a63afdc99925d46f707aa04d671dda361f8737e
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2024-04-14 11:52:28 -0300

    arregle card de proyecto se rompia en responsive boton y les puse mismo tamaño a todas

--- src/Components/CardProyects/CardsProyects.jsx
+++ src/Components/CardProyects/CardsProyects.jsx
@@ -10,12 +10,14 @@ function CardsProyects({info, img}) {
       <Card.Img variant="top" src={img}/>
       <Card.Body>
         <Card.Title><b>{info.tittle}</b></Card.Title>
+        <div id="textBoton">
         <Card.Text>
           {info.description}
           <br />
           {info && info.deploy ? <a href={info.deploy}>Ir al sitio</a> : ""}
         </Card.Text>
         <Button href={info.URL} variant="primary" target="_blank">Repositorio</Button>
+        </div>
       </Card.Body>
     </Card>
   );
